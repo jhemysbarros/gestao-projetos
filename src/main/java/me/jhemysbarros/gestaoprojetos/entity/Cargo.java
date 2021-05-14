aae8351610ec1323529f3b64df5f44a9f9451536
Jhemys <jhemysbarros@gmail.com>
2021-05-14 18:01:02 -0300

    refact: Cliente e Funcionario -> Pessoa e Entidade

---
+++ src/main/java/me/jhemysbarros/gestaoprojetos/entity/Cargo.java
@@ -0,0 +1,17 @@
+// package me.jhemysbarros.gestaoprojetos.entity;
+
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.swing.Spring;
+
+// @Entity
+// public class Cargo {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     private Spring nome;
+// }
