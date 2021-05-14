b2f4c00437fcc274f42f0f08653083217e51c42c
Jhemys <jhemysbarros@gmail.com>
2021-05-14 12:01:24 -0300

    git commit -m "feat: start inicial do projeto."

---
+++ src/main/java/me/jhemysbarros/gestaoprojetos/GestaoProjetosApplication.java
@@ -0,0 +1,13 @@
+package me.jhemysbarros.gestaoprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProjetosApplication.class, args);
+	}
+
+}
