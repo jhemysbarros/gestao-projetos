7e218e2db66a35706462194d71233125c3cf3b34
Jhemys Barros <jhemysbarros@gmail.com>
2021-06-03 00:57:01 -0300

    feat: implementação do UserDetails e UserDetailsService.

--- src/main/java/me/jhemysbarros/gestaoprojetos/repository/FuncionarioRepository.java
+++ src/main/java/me/jhemysbarros/gestaoprojetos/repository/FuncionarioRepository.java
@@ -1,6 +1,7 @@
 package me.jhemysbarros.gestaoprojetos.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,7 @@ import me.jhemysbarros.gestaoprojetos.entity.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
-    @EntityGraph(attributePaths = {"endereco", "cargo"})
+    @EntityGraph(attributePaths = { "endereco", "cargo" })
     List<Funcionario> findAll();
 
     @Query("select f from Funcionario f where f.cargo.nome = :cargoNome")
@@ -22,4 +23,6 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> 
     List<Funcionario> findByCargoNome(String cargoNome);
 
     List<Funcionario> findByCargoNomeNot(String cargoNome);
+
+    Optional<Funcionario> findByEmail(String email);
 }
---
+++ src/main/java/me/jhemysbarros/gestaoprojetos/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package me.jhemysbarros.gestaoprojetos.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import me.jhemysbarros.gestaoprojetos.entity.Funcionario;
+import me.jhemysbarros.gestaoprojetos.entity.UserDetailsImpl;
+import me.jhemysbarros.gestaoprojetos.repository.FuncionarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Funcionario funcionario = funcionarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new UserDetailsImpl(funcionario);
+    }
+}
