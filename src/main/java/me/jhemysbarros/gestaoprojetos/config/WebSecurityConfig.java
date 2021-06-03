743518edc6250cfb84ae62fb80c60c9bc90ea8ea
Jhemys Barros <jhemysbarros@gmail.com>
2021-06-03 01:26:24 -0300

    feat: implementação do WebSecurityConfig.

---
+++ src/main/java/me/jhemysbarros/gestaoprojetos/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package me.jhemysbarros.gestaoprojetos.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import me.jhemysbarros.gestaoprojetos.service.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/adminlte/**").permitAll()
+            .antMatchers("/img/**").permitAll()
+            .antMatchers("/js/**").permitAll()
+            .antMatchers("/plugins/**").permitAll()
+            .anyRequest().authenticated();
+
+        http.formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/clientes")
+            .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImpl)
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }    
+}
